d7d2d56e4ff214e282f8fd4cfea86c37320db06d
gr3gorycode <94622012+gr3gorycode@users.noreply.github.com>
2021-11-18 09:51:22 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from streamlit_folium import folium_static
+import folium
+import geopandas as gpd
+import json
+import pydeck as pdk
+import geemap.eefolium as geemap
+
+uploaded_file = st.sidebar.file_uploader(label="Upload GeoJSON file", type=['geojson'], accept_multiple_files=False)
+if uploaded_file is not None:
+    data = json.load(uploaded_file)
+    gdf = gpd.GeoDataFrame.from_features(data['features'])[['geometry']]
+    point = data['features'][0]['geometry']['coordinates'][0][0][0]
+
+    st.pydeck_chart(pdk.Deck(
+        map_provider="mapbox",
+        map_style=pdk.map_styles.SATELLITE,
+        api_keys  = { "mapbox": "pk.eyJ1IjoiZ3JvbDIwMjAiLCJhIjoiY2tvajI0MW5pMDMxMDJ3bzdpN3dzbHBidyJ9.jxYSuCXT1u1eFKMMWwSFVg"},
+        initial_view_state=pdk.ViewState(
+            latitude  = 51.6693206,
+            longitude = -9.002953,
+            zoom=12,
+            pitch=0,
+        ),
+        layers=[
+            pdk.Layer(
+                'GeoJsonLayer',
+                data=gdf,
+                get_fill_color=[0, 0, 0],
+            ),
+        ],
+    ))
+    st.write()
+
+    
+#     st.write(gpd.read_file(uploaded_file))
+    # # center on Liberty Bell
+    # m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+    # folium.GeoJson(uploaded_file, name="geojson").add_to(m)
+    # # add marker for Liberty Bell
+    # tooltip = "Liberty Bell"
+    # folium.Marker(
+    #     [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+    # ).add_to(m)
+
+    # # call to render Folium map in Streamlit
+    # folium_static(m)
+
